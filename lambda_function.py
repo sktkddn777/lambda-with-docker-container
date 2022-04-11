cd8ff4375f198d6a52629ec421f30f91343edd8b
sktkddn777 <sktkddn777@naver.com>
2022-04-12 02:16:44 +0900

    lambda_handler with s3

--- lambda_function.py
+++ lambda_function.py
@@ -6,76 +6,67 @@ from utils.general import non_max_suppression, scale_coords
 from utils.plots import Annotator
 
 import boto3
+import urllib.parse
+import uuid
 
-# AWS account
-ACCESS_KEY = {IAM Access Key 입력}
-SECRET_KEY = {IAM Secret Access Key 입력}
-BUCKET_NAME = {S3 Bucket 이름 입력}
-URI = {S3 uri 입력}
+s3 = boto3.client('s3')
 
-resource = boto3.resource(
-    's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY
-)
+def apply_yolo(img_path, upload_path):
+  # Model Setting
+  MODEL_PATH = '/var/task/weights/e50b32.pt'
 
-bucket = resource.Bucket(BUCKET_NAME)
+  img_size = 416
+  conf_thres = 0.5  # confidence threshold
+  iou_thres = 0.45  # NMS IOU threshold
+  max_det = 1000  # maximum detections per image
+  classes = None  # filter by class
+  agnostic_nms = False  # class-agnostic NMS
 
-s3 = boto3.resource(
-    's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY
-)
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-bucket = s3.Bucket(BUCKET_NAME)
+  ckpt = torch.load(MODEL_PATH, map_location=device)
+  model = ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval()
+  class_names = ['fire'] # model.names
+  stride = int(model.stride.max())
+  colors = ((0, 255, 0)) # (gray, red, green)
 
-# Model Setting
-MODEL_PATH = 'weights/e50b32.pt'
+  img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
-img_size = 416
-conf_thres = 0.5  # confidence threshold
-iou_thres = 0.45  # NMS IOU threshold
-max_det = 1000  # maximum detections per image
-classes = None  # filter by class
-agnostic_nms = False  # class-agnostic NMS
+  img_input = letterbox(img, img_size, stride=stride)[0]
+  img_input = img_input.transpose((2, 0, 1))[::-1]
+  img_input = np.ascontiguousarray(img_input)
+  img_input = torch.from_numpy(img_input).to(device)
+  img_input = img_input.float()
+  img_input /= 255.
+  img_input = img_input.unsqueeze(0)
 
-img_test = '0.jpg'
+  pred = model(img_input, augment=False, visualize=False)[0]
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  # postprocess
+  pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
+  pred = pred.cpu().numpy()
 
-ckpt = torch.load(MODEL_PATH, map_location=device)
-model = ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval()
-class_names = ['fire'] # model.names
-stride = int(model.stride.max())
-colors = ((0, 255, 0)) # (gray, red, green)
+  pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], img.shape).round()
+  annotator = Annotator(img.copy(), line_width=3, example=str(class_names), font='data/malgun.ttf')
 
-img = cv2.imread(img_test, cv2.IMREAD_COLOR)
+  for p in pred:
+          class_name = class_names[int(p[5])]
 
-img_input = letterbox(img, img_size, stride=stride)[0]
-img_input = img_input.transpose((2, 0, 1))[::-1]
-img_input = np.ascontiguousarray(img_input)
-img_input = torch.from_numpy(img_input).to(device)
-img_input = img_input.float()
-img_input /= 255.
-img_input = img_input.unsqueeze(0)
+          x1, y1, x2, y2 = p[:4]
+          print(p)
+          annotator.box_label([x1, y1, x2, y2], '%s %d' % (class_name, float(p[4]) * 100), color=colors[int(p[5])])
 
-pred = model(img_input, augment=False, visualize=False)[0]
+  result_img = annotator.result()
+  cv2.imwrite(upload_path, result_img)
+  return
 
-# postprocess
-pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
-pred = pred.cpu().numpy()
+def lambda_handler(event, context):
+  bucket = event['Records'][0]['s3']['bucket']['name']
+  key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+  tmpkey=key.replace('/','')
+  download_path='/var/task/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+  upload_path='/var/task/tmp/model-{}'.format(tmpkey)
 
-pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], img.shape).round()
-annotator = Annotator(img.copy(), line_width=3, example=str(class_names), font='data/malgun.ttf')
-
-for p in pred:
-        class_name = class_names[int(p[5])]
-
-        x1, y1, x2, y2 = p[:4]
-        print(p)
-        annotator.box_label([x1, y1, x2, y2], '%s %d' % (class_name, float(p[4]) * 100), color=colors[int(p[5])])
-
-result_img = annotator.result()
-cv2.imwrite('result.png', result_img)
-# results = model(img)
-# results.show()
+  s3.download_file(bucket, key, download_path)
+  apply_yolo(download_path, upload_path)
+  s3.upload_file(upload_path,'{}-model'.format(bucket), key)
